b0b47f227edad2f1a8f461c9d9c4cb031ba6a712
Azim Salikhov <salikhovazim@gmail.com>
2017-07-11 15:17:04 +0000

    Added create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708110051) do
+ActiveRecord::Schema.define(version: 20170708105035) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
